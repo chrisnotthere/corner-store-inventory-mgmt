4cc7fcb9022ac6ee8338b52da7d8ed936a1ed498
chrisnotthere <mr.chrisnash@gmail.com>
2021-12-22 10:40:28 -0800

    product list

--- controllers/productController.js
+++ controllers/productController.js
@@ -23,9 +23,21 @@ exports.index = function (req, res) {
   );
 };
 
-// Display list of all products.
-exports.product_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: product list");
+// Display list of all Products.
+exports.product_list = function (req, res, next) {
+  Product.find({}, "name category price")
+    .sort({ name: 1 })
+    .populate("category")
+    .exec(function (err, list_products) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render("product_list", {
+        title: "Product List",
+        product_list: list_products,
+      });
+    });
 };
 
 // Display detail page for a specific product.
