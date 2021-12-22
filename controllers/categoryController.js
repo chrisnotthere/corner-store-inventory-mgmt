b51d95071e56b79796a388f00601cba1a4fac625
chrisnotthere <mr.chrisnash@gmail.com>
2021-12-22 10:50:28 -0800

    basic category list

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,41 +1,52 @@
-var Category = require('../models/category');
+var Category = require("../models/category");
 
-// Display list of all categorys.
-exports.category_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: category list');
+// Display list of all Categories.
+exports.category_list = function (req, res, next) {
+  Category.find()
+    .sort([["name", "ascending"]])
+    .exec(function (err, list_categories) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render("category_list", {
+        title: "Category List",
+        category_list: list_categories,
+      });
+    });
 };
 
 // Display detail page for a specific category.
-exports.category_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: category detail: ' + req.params.id);
+exports.category_detail = function (req, res) {
+  res.send("NOT IMPLEMENTED: category detail: " + req.params.id);
 };
 
 // Display category create form on GET.
-exports.category_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: category create GET');
+exports.category_create_get = function (req, res) {
+  res.send("NOT IMPLEMENTED: category create GET");
 };
 
 // Handle category create on POST.
-exports.category_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: category create POST');
+exports.category_create_post = function (req, res) {
+  res.send("NOT IMPLEMENTED: category create POST");
 };
 
 // Display category delete form on GET.
-exports.category_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: category delete GET');
+exports.category_delete_get = function (req, res) {
+  res.send("NOT IMPLEMENTED: category delete GET");
 };
 
 // Handle category delete on POST.
-exports.category_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: category delete POST');
+exports.category_delete_post = function (req, res) {
+  res.send("NOT IMPLEMENTED: category delete POST");
 };
 
 // Display category update form on GET.
-exports.category_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: category update GET');
+exports.category_update_get = function (req, res) {
+  res.send("NOT IMPLEMENTED: category update GET");
 };
 
 // Handle category update on POST.
-exports.category_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: category update POST');
+exports.category_update_post = function (req, res) {
+  res.send("NOT IMPLEMENTED: category update POST");
 };
