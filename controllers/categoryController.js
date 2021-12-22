f24a71f7c8f9607c945d389f662e6abca80b2040
chrisnotthere <mr.chrisnash@gmail.com>
2021-12-22 15:07:20 -0800

    working on add category form

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,6 +2,8 @@ var async = require("async");
 var Product = require("../models/product");
 var Category = require("../models/category");
 
+const { body,validationResult } = require('express-validator');
+
 // Display list of all Categories.
 exports.category_list = function (req, res, next) {
   Category.find()
@@ -30,7 +32,7 @@ exports.category_detail = function (req, res, next) {
       },
     },
     function (err, results) {
-      console.dir(results)
+      console.dir(results);
       if (err) {
         return next(err);
       } // Error in API usage.
@@ -49,15 +51,62 @@ exports.category_detail = function (req, res, next) {
   );
 };
 
-// Display category create form on GET.
-exports.category_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: category create GET");
+// Display Category create form on GET.
+exports.category_create_get = function (req, res, next) {
+  res.render("category_form", { title: "Create Category" });
 };
 
-// Handle category create on POST.
-exports.category_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: category create POST");
-};
+// Handle Category create on POST.
+exports.category_create_post = [
+  // Validate and sanitize fields.
+  body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Name must be specified."),
+  body("description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Description must be specified."),
+  body("image")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Image must be specified."),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("category_form", {
+        title: "Create Category",
+        category: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Create a Category object with escaped and trimmed data.
+      var category = new Category({
+        name: req.body.name,
+        description: req.body.description,
+        image: req.body.image,
+      });
+      category.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new category record.
+        res.redirect(category.url);
+      });
+    }
+  },
+];
 
 // Display category delete form on GET.
 exports.category_delete_get = function (req, res) {
